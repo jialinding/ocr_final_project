65610036ed98b6514cc6f38c9bceb7aff9962cec
Yishen Chen <ychen306@mit.edu>
2019-11-26 17:09:23 -0500

    add image generator

---
+++ src/gen-images.py
@@ -0,0 +1,44 @@
+from utils import gen_image
+import random
+import sys
+import cv2
+import os
+from tqdm import tqdm
+
+counter = 0
+
+qualities = list(range(1, 20, 2))
+
+# TODO: perturb the generated image
+def output_chunk(source_name, lines, output_dir):
+  global counter
+  name = '%s_%d' % (source_name, counter)
+  counter += 1
+
+  img = gen_image(lines)
+
+  # write the generated image with various quality
+  for qual in qualities:
+    out_name = '%s-%d.jpg' % (name, qual)
+    cv2.imwrite(os.path.join(output_dir, out_name), img, (cv2.IMWRITE_JPEG_QUALITY, qual))
+
+  # write the text, which will be the labels.
+  with open(os.path.join(output_dir, name+'.txt'), 'w') as text_f:
+    for line in lines:
+      text_f.write(line+'\n')
+
+input_f, output_dir = sys.argv[1:]
+
+min_chunk_size = 25
+max_chunk_size = 100
+
+chunk = []
+chunk_size = random.randint(min_chunk_size, max_chunk_size)
+filename = os.path.basename(input_f)
+with open(input_f) as f:
+  for line in tqdm(list(f)):
+    chunk.append(line[:-1]) # strip '\n'
+    if len(chunk) == chunk_size:
+      output_chunk(filename, chunk, output_dir)
+      chunk_size = random.randint(min_chunk_size, max_chunk_size)
+      chunk = []
---
+++ src/utils.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+import random
+
+font_face_pool = [
+    cv2.FONT_HERSHEY_SIMPLEX,
+    cv2.FONT_HERSHEY_PLAIN,
+    cv2.FONT_HERSHEY_DUPLEX,
+    cv2.FONT_HERSHEY_COMPLEX,
+    cv2.FONT_HERSHEY_TRIPLEX,
+    cv2.FONT_HERSHEY_COMPLEX_SMALL,
+    ]
+font_scale_pool = list(range(1, 5))
+thickness_pool = list(range(2, 4))
+color_pool = [(0,255), (255,0)]
+
+def gen_image(lines):
+  font_face = random.choice(font_face_pool)
+  font_scale = random.choice(font_scale_pool)
+  thickness = random.choice(thickness_pool)
+  text_c, background_c = random.choice(color_pool)
+
+  # figure out size of the image
+  ws = []
+  hs = []
+  for line in lines:
+    (w, h), _ = cv2.getTextSize(line, font_face, font_scale, thickness)
+    ws.append(w)
+    hs.append(h)
+  w = max(ws)
+  h = sum(hs)
+
+  img = np.zeros((int(h*2),int(w*1.5),3), dtype=np.uint8)
+  img[:,:,:] = background_c
+
+  y = 0
+  for line, h2 in zip(lines, hs):
+    y += int(h2 * 1.5)
+    cv2.putText(img, line, (int(0.25*w), y), font_face, font_scale, (text_c,text_c,text_c), thickness)
+  return img
